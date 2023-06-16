c0be8c79aa12eb446fee86343b6e5179c44e2393
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-06-16 14:42:14 +0000

    Render Statistics

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+type ValueProps = {
+  label: string;
+  value: string;
+};
+
+const Value = ({ label, value }: ValueProps) => (
+  <View style-={styles.valueContainer}>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{ flexDirection: "row" }}>
+        <Value label="Steps" value="1212 " />
+        <Value label="Distance" value="1212 km" />
+      </View>
+      <Value label="Flight Climbed" value="0.6km" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    justifyContent: "center",
+    padding: 12,
+  },
+  label: {
+    color: "white",
+    fontSize: 20,
+  },
+  value: {
+    fontSize: 35,
+    color: "#afb3be",
+    fontWeight: "500",
+  },
+  valueContainer: {
+    marginRight: 50,
+    marginVertical: 10,
   },
 });
