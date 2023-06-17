91d16a53cbe4653895394f4b437805caa9cb73dd
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-06-17 10:41:38 +0000

    Apple healthkit only for ios not android

--- src/hooks/useHealthData.tsx
+++ src/hooks/useHealthData.tsx
@@ -5,6 +5,7 @@ import AppleHealthKit, {
 } from "react-native-health";
 import { useEffect, useState } from "react";
 import appleHealthKit from "react-native-health";
+import { Platform } from "react-native";
 const permissions: HealthKitPermissions = {
   permissions: {
     read: [
@@ -15,18 +16,32 @@ const permissions: HealthKitPermissions = {
     write: [],
   },
 };
-const useHealthData = (date:Date) => {
+const useHealthData = (date: Date) => {
   const [hasPermissions, setHasPermissions] = useState(false);
   const [steps, setSteps] = useState(0);
   const [flights, setFlight] = useState(0);
 
   const [distance, setDistance] = useState(0);
   useEffect(() => {
-    AppleHealthKit.initHealthKit(permissions, (err) => {
+    if (Platform.OS !== "ios") {
+      return;
+    }
+
+    AppleHealthKit.isAvailable((err, isAvailable) => {
       if (err) {
-        console.log("eror gettinh permission");
+        console.log("Error checking availability");
+        return;
       }
-      setHasPermissions(true);
+      if (!isAvailable) {
+        console.log("Apple Health is not available");
+        return;
+      }
+      AppleHealthKit.initHealthKit(permissions, (err) => {
+        if (err) {
+          console.log("eror gettinh permission");
+        }
+        setHasPermissions(true);
+      });
     });
   }, []);
 
